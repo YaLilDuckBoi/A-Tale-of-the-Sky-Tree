35a0dffa331db47a952006730de83ac3a5d3c8d5
Duck <68995955+YaLilDuckBoi@users.noreply.github.com>
2021-10-10 21:32:54 +0200

    Tiles, code, sprites

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -8,8 +8,10 @@ var MOVESPEED = 400
 var GRAVITY = 20
 var MAXFALLSPEED = 650
 var FASTFALLSPEED = 100
-var MAXAIRJUMPS = 2000000000
+var MAXAIRJUMPS = 2
 var AIRJUMPS = MAXAIRJUMPS
+##var ISFLOORJUMPOK := true
+
 
 func _physics_process(delta):
 	
@@ -22,6 +24,14 @@ func _physics_process(delta):
 		motion.x = -MOVESPEED
 	else:
 		motion.x = 0
+		
+	##if is_on_floor():
+		##ISFLOORJUMPOK = true
+	##else:
+		##ISFLOORJUMPOK = false
+		##get_node("Coytime").start()
+		
+	##if ISFLOORJUMPOK:
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			motion.y = JUMPHEIGHT
@@ -39,3 +49,8 @@ func _physics_process(delta):
 		motion.y = MAXFALLSPEED
 	motion =  move_and_slide(motion, UP)
 
+
+
+##func _on_Coytime_timeout():
+	##print("Coytime time out")
+	##ISFLOORJUMPOK = false
